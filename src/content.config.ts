029cb74743fb7ca9062f6c4a8560c271eb3bda9b
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2025-09-13 23:29:06 +0200

    Add properties to project schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,21 @@ import { defineCollection, z } from "astro:content";
 
 import { glob } from "astro/loaders";
 
+export const ProjectStatus = {
+  live: "live",
+  inProgress: "inProgress",
+  outdated: "outdated",
+} as const;
+
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    image: z.string().optional(),
+    image: z.string(),
+    status: z.nativeEnum(ProjectStatus),
+    repoLink: z.string().optional(),
+    liveLink: z.string().optional(),
   }),
 });
 
